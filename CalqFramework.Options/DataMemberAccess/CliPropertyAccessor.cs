bb9bc6988e68cf24359d6d3674a3938e7da51b49
greg-chuchro <gchuchro@acm.org>
2023-12-07 09:55:41 +0900

    --help

--- CalqFramework.Options/DataMemberAccess/CliPropertyAccessor.cs
+++ CalqFramework.Options/DataMemberAccess/CliPropertyAccessor.cs
@@ -1,13 +1,47 @@
 ﻿using CalqFramework.Options.Attributes;
 using CalqFramework.Serialization.DataMemberAccess;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace CalqFramework.Options.DataMemberAccess {
+    // TODO unify with CliFieldAccessor
     public class CliPropertyAccessor : PropertyAccessorBase {
         public CliPropertyAccessor(BindingFlags bindingAttr) : base(bindingAttr) {
         }
 
+        public override IDictionary<string, MemberInfo> GetDataMembersByKeys(Type type) {
+            var result = new Dictionary<string, MemberInfo>();
+            var membersWithShortNames = new HashSet<MemberInfo>();
+            var shortNameDuplicates = new HashSet<string>();
+            var members = type.GetProperties(BindingAttr);
+            foreach (var member in members) {
+                var name = member.GetCustomAttribute<NameAttribute>()?.Name;
+                if (name != null) {
+                    result[name] = member;
+                } else {
+                    result[member.Name] = member;
+                }
+                var shortName = member.GetCustomAttribute<ShortNameAttribute>()?.Name;
+                if (shortName != null) {
+                    result[shortName.ToString()!] = member;
+                    membersWithShortNames.Add(member);
+                }
+            }
+            foreach (var member in members) {
+                if (!membersWithShortNames.Contains(member)) {
+                    if (result.TryAdd(member.Name[0].ToString(), member) == false) {
+                        shortNameDuplicates.Add(member.Name[0].ToString());
+                    };
+                }
+            }
+            foreach (var duplicate in shortNameDuplicates) {
+                result.Remove(duplicate);
+            }
+            return result;
+        }
+
+        // FIXME do not assign the first occurances - check for duplicates. if duplicate found then then return null
         public override MemberInfo? GetDataMember(Type type, string dataMemberKey) {
             if (dataMemberKey.Length == 1) {
                 foreach (var member in type.GetProperties(BindingAttr)) {
