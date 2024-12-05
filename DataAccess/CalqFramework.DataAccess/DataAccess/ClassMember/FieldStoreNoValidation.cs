439f6cdcd90df330373cc97d0828829410f683de
greg-chuchro <gchuchro@acm.org>
2024-12-06 03:26:45 +0900

    dereference CalqFramework.Serialization

---
+++ DataAccess/CalqFramework.DataAccess/DataAccess/ClassMember/FieldStoreNoValidation.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace CalqFramework.Serialization.DataAccess.ClassMember
+{
+    sealed public class FieldStoreNoValidation : FieldStoreBase<string> {
+        public FieldStoreNoValidation(object obj, BindingFlags bindingAttr) : base(obj, bindingAttr) {
+        }
+
+        protected override MemberInfo? GetClassMember(string key) {
+            return ParentType.GetField(key, BindingAttr);
+        }
+
+        public override bool ContainsAccessor(MemberInfo accessor) {
+            return true;
+        }
+    }
+}
---
+++ DataAccess/CalqFramework.DataAccess/DataAccess/ClassMember/PropertyStore.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace CalqFramework.Serialization.DataAccess.ClassMember {
+    sealed public class PropertyStore : PropertyStoreBase<string> {
+        public PropertyStore(object obj, BindingFlags bindingAttr) : base(obj, bindingAttr) {
+        }
+
+        protected override MemberInfo? GetClassMember(string key) {
+            return ParentType.GetProperty(key, BindingAttr);
+        }
+
+        public override bool ContainsAccessor(MemberInfo accessor) {
+            return accessor is PropertyInfo && accessor.DeclaringType == ParentType;
+        }
+    }
+}
