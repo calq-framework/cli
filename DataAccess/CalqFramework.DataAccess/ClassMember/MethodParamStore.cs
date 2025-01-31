83f72e2e3ae7e27c0d361883eb849dfd979d6a29
greg-chuchro <gchuchro@acm.org>
2025-02-01 08:35:23 +0900

    MethodParamStore

---
+++ DataAccess/CalqFramework.DataAccess/ClassMember/MethodParamStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace CalqFramework.DataAccess.ClassMember {
+    public class MethodParamStore : MethodParamStoreBase<string> {
+        public MethodParamStore(MethodInfo method) : base(method) {
+        }
+
+        public override bool ContainsAccessor(ParameterInfo accessor) {
+            return accessor.Member == Method;
+        }
+
+        public override bool TryGetAccessor(string key, [MaybeNullWhen(false)] out ParameterInfo result) {
+            result = Parameters.FirstOrDefault(x => x.Name== key);
+            return result != null;
+        }
+    }
+}
