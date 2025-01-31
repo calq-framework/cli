e2b9b58967405c5bc090ff6e07a50edf8b0a2d0a
greg-chuchro <gchuchro@acm.org>
2025-01-31 13:24:15 +0900

    ClassDataMemberStoreBase

---
+++ DataAccess/CalqFramework.DataAccess/ClassMember/ClassDataMemberStoreBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace CalqFramework.DataAccess.ClassMember {
+    public abstract class ClassDataMemberStoreBase<TKey, TValue> : KeyValueStoreBase<TKey, TValue, MemberInfo> {
+
+        public ClassDataMemberStoreBase(object obj, BindingFlags bindingAttr) {
+            ParentObject = obj;
+            BindingAttr = bindingAttr;
+            ParentType = obj.GetType();
+        }
+
+        protected object ParentObject { get; }
+        protected Type ParentType { get; }
+        protected BindingFlags BindingAttr { get; }
+
+        protected override MissingMemberException CreateMissingMemberException(TKey key) {
+            return new MissingMemberException($"Missing {key} in {ParentType}.");
+        }
+    }
+}
---
+++ DataAccess/CalqFramework.DataAccess/KeyAccessorResolverBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace CalqFramework.DataAccess {
+    public abstract class KeyAccessorResolverBase<TKey, TValue> : IKeyAccessorResolver<TKey, TValue> {
+        public TValue GetAccessor(TKey key) {
+            return TryGetAccessor(key, out var result) ? result : throw CreateMissingMemberException(key);
+        }
+
+        public abstract bool TryGetAccessor(TKey key, [MaybeNullWhen(false)] out TValue result);
+
+        protected abstract MissingMemberException CreateMissingMemberException(TKey key);
+    }
+}
