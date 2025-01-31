e1ec26e04d83a997fad90bf4bdb2fb90561cfd39
greg-chuchro <gchuchro@acm.org>
2025-02-01 08:35:23 +0900

    MethodParamStore

---
+++ DataAccess/CalqFramework.DataAccess/ClassMember/MethodParamStoreNoValidation.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace CalqFramework.DataAccess.ClassMember {
+    public class MethodParamStoreNoValidation : MethodParamStoreBase<string> {
+        public MethodParamStoreNoValidation(MethodInfo method) : base(method) {
+        }
+
+        public override bool ContainsAccessor(ParameterInfo accessor) {
+            return true;
+        }
+
+        public override bool TryGetAccessor(string key, [MaybeNullWhen(false)] out ParameterInfo result) {
+            result = Parameters.FirstOrDefault(x => x.Name== key);
+            return result != null;
+        }
+    }
+}
