a432ef550b2447d8f3c2fc79f55d682bd4c5b245
greg-chuchro <gchuchro@acm.org>
2025-02-13 22:50:15 +0900

    IReadOnlyKeyValueStore

--- DataAccess/CalqFramework.DataAccess/IKeyValueStore.cs
+++ DataAccess/CalqFramework.DataAccess/IKeyValueStore.cs
@@ -1,18 +1,14 @@
 ﻿namespace CalqFramework.DataAccess {
 
-    public interface IKeyValueStore<TKey, TValue> {
-        public TValue this[TKey key] { get; set; }
-
-        bool ContainsKey(TKey key);
-
-        Type GetDataType(TKey key);
+    public interface IKeyValueStore<TKey, TValue> : IReadOnlyKeyValueStore<TKey, TValue> {
+        new TValue this[TKey key] { get; set; }
 
         TValue GetValueOrInitialize(TKey key);
     }
 
-    public interface IKeyValueStore<TKey, TValue, TAccessor> : IKeyValueStore<TKey, TValue, TAccessor, TValue> {
+    public interface IKeyValueStore<TKey, TValue, TAccessor, TInternalValue> : IKeyAccessorResolver<TKey, TAccessor>, IKeyValueStore<TKey, TValue>, IMediatedKeyValueStore<TAccessor, TInternalValue> {
     }
 
-    public interface IKeyValueStore<TKey, TValue, TAccessor, TInternalValue> : IKeyAccessorResolver<TKey, TAccessor>, IKeyValueStore<TKey, TValue>, IMediatedKeyValueStore<TAccessor, TInternalValue> {
+    public interface IKeyValueStore<TKey, TValue, TAccessor> : IKeyValueStore<TKey, TValue, TAccessor, TValue> {
     }
 }
---
+++ DataAccess/CalqFramework.DataAccess/IReadOnlyKeyValueStore.cs
@@ -0,0 +1,10 @@
+﻿namespace CalqFramework.DataAccess {
+
+    public interface IReadOnlyKeyValueStore<TKey, TValue> {
+        TValue this[TKey key] { get; }
+
+        bool ContainsKey(TKey key);
+
+        Type GetDataType(TKey key);
+    }
+}
