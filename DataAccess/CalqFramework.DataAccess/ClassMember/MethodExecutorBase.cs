13c9583a7d2d07d5f263b43cc690e75ec16c1a67
greg-chuchro <gchuchro@acm.org>
2025-02-13 22:49:56 +0900

    MethodExecutor

---
+++ DataAccess/CalqFramework.DataAccess/ClassMember/MethodExecutorBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace CalqFramework.DataAccess.ClassMember {
+    public abstract class MethodExecutorBase<TParameterKey, TParameterValue> : ParameterStoreBase<TParameterKey, TParameterValue>, IFunctionExecutor<TParameterKey, TParameterValue> {
+        protected MethodExecutorBase(MethodInfo method, object obj) : base(method) {
+            ParentObject = obj;
+        }
+
+        public object ParentObject { get; }
+
+        public abstract void AddParameter(TParameterValue value);
+
+        public object? Invoke() {
+            return Method.Invoke(ParentObject, ParameterValues);
+        }
+    }
+}
---
+++ DataAccess/CalqFramework.DataAccess/IFunctionExecutor.cs
@@ -0,0 +1,7 @@
+﻿namespace CalqFramework.DataAccess {
+
+    public interface IFunctionExecutor<TParameterKey, TParameterValue> : IKeyValueStore<TParameterKey, TParameterValue> {
+        void AddParameter(TParameterValue value);
+        object? Invoke();
+    }
+}
