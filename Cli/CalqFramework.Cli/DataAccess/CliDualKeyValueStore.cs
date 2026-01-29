3ea245a1449f45b29e803a6f41d67fd840594d54
greg-chuchro <gchuchro@acm.org>
2026-01-29 16:25:07 +0100

    refactoring

--- Cli/CalqFramework.Cli/DataAccess/CliDualKeyValueStore.cs
+++ Cli/CalqFramework.Cli/DataAccess/CliDualKeyValueStore.cs
@@ -7,19 +7,19 @@ namespace CalqFramework.Cli.DataAccess {
 
     internal class CliDualKeyValueStore<TValue> : DualKeyValueStoreBase<string, TValue>, ICliKeyValueStore<string, TValue, MemberInfo> {
         private readonly ICliKeyValueStore<string, TValue, MemberInfo> _primaryStore;
-        private readonly ICliKeyValueStore<string, TValue, MemberInfo> _secondarStore;
+        private readonly ICliKeyValueStore<string, TValue, MemberInfo> _secondaryStore;
 
         public CliDualKeyValueStore(ICliKeyValueStore<string, TValue, MemberInfo> primaryStore, ICliKeyValueStore<string, TValue, MemberInfo> secondaryStore) {
             _primaryStore = primaryStore;
-            _secondarStore = secondaryStore;
+            _secondaryStore = secondaryStore;
         }
 
         protected override IKeyValueStore<string, TValue> PrimaryStore => _primaryStore;
 
-        protected override IKeyValueStore<string, TValue> SecondaryStore => _secondarStore;
+        protected override IKeyValueStore<string, TValue> SecondaryStore => _secondaryStore;
 
         public IDictionary<MemberInfo, IEnumerable<string>> GetKeysByAccessors() {
-            return _primaryStore.GetKeysByAccessors().Concat(_secondarStore.GetKeysByAccessors()).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            return _primaryStore.GetKeysByAccessors().Concat(_secondaryStore.GetKeysByAccessors()).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
         }
     }
 }
