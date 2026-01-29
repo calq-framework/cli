d33be6eaeb7761e19fd1316f69f5753cb9274dd3
greg-chuchro <gchuchro@acm.org>
2026-01-30 00:20:55 +0100

    min max exception via reflection

--- Cli/CalqFramework.Cli/Parsing/ArgValueParser.cs
+++ Cli/CalqFramework.Cli/Parsing/ArgValueParser.cs
@@ -27,58 +27,15 @@ public class ArgValueParser : IStringParser {
         try {
             return _stringParser.ParseValue(value, type);
         } catch (OverflowException ex) {
-            long min;
-            ulong max;
-            switch (Type.GetTypeCode(type)) {
-                case TypeCode.Byte:
-                    min = byte.MinValue;
-                    max = byte.MaxValue;
-                    break;
-
-                case TypeCode.SByte:
-                    min = sbyte.MinValue;
-                    max = (ulong)sbyte.MaxValue;
-                    break;
-
-                case TypeCode.Char:
-                    min = char.MinValue;
-                    max = char.MaxValue;
-                    break;
-
-                case TypeCode.Int32:
-                    min = int.MinValue;
-                    max = int.MaxValue;
-                    break;
-
-                case TypeCode.UInt32:
-                    min = uint.MinValue;
-                    max = uint.MaxValue;
-                    break;
-
-                case TypeCode.Int64:
-                    min = long.MinValue;
-                    max = long.MaxValue;
-                    break;
-
-                case TypeCode.UInt64:
-                    min = (long)ulong.MinValue;
-                    max = ulong.MaxValue;
-                    break;
-
-                case TypeCode.Int16:
-                    min = short.MinValue;
-                    max = (ulong)short.MaxValue;
-                    break;
-
-                case TypeCode.UInt16:
-                    min = ushort.MinValue;
-                    max = ushort.MaxValue;
-                    break;
-
-                default:
-                    throw;
+            var minField = type.GetField("MinValue");
+            var maxField = type.GetField("MaxValue");
+            
+            if (minField != null && maxField != null) {
+                var min = minField.GetValue(null);
+                var max = maxField.GetValue(null);
+                throw new ArgValueParserException($"out of range ({min}-{max})", ex);
             }
-            throw new ArgValueParserException($"out of range ({min}-{max})", ex);
+            throw;
         } catch (FormatException ex) {
             throw new ArgValueParserException($"invalid format (expected {type.Name})", ex);
         }
