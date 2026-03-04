c565c331a5c3e05eb818fe5d910163a2a7c6eed5
greg-chuchro <gchuchro@acm.org>
2026-03-04 11:51:02 +0900

    UTF-8

--- CalqFramework.Cli/DataAccess/InterfaceComponentStores/ISubcommandExecutorWithOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CalqFramework.Cli.DataAccess.InterfaceComponentStores;
-
-/// <summary>
-///     Executes CLI subcommands with both parameter and option support (enables shadowing).
-/// </summary>
-public interface ISubcommandExecutorWithOptions : ISubcommandExecutor, IOptionStore {
-}
