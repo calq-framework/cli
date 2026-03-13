23c228a14e3a7f9870f044fa9375c8d213d66e3e
greg-chuchro <gchuchro@acm.org>
2026-03-13 19:01:39 +0900

    fix MoveFieldAttributesToProperty

--- CalqFramework.DataAccess/DualKeyValueStore.cs
+++ CalqFramework.DataAccess/DualKeyValueStore.cs
@@ -4,10 +4,7 @@
 ///     Combines two key-value stores with primary and secondary lookup priority.
 /// </summary>
 public class DualKeyValueStore<TKey, TValue>(IKeyValueStore<TKey, TValue> primaryStore, IKeyValueStore<TKey, TValue> secondaryStore) : DualKeyValueStoreBase<TKey, TValue> {
-    private readonly IKeyValueStore<TKey, TValue> _primaryStore = primaryStore;
-    private readonly IKeyValueStore<TKey, TValue> _secondaryStore = secondaryStore;
+    protected override IKeyValueStore<TKey, TValue> PrimaryStore { get; } = primaryStore;
 
-    protected override IKeyValueStore<TKey, TValue> PrimaryStore => _primaryStore;
-
-    protected override IKeyValueStore<TKey, TValue> SecondaryStore => _secondaryStore;
+    protected override IKeyValueStore<TKey, TValue> SecondaryStore { get; } = secondaryStore;
 }
